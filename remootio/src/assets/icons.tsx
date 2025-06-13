cf606ab41ee9ed604f90e2cff723ea21ffcb9a54
Matevz <semprimoznik.matevz@gmail.com>
2025-06-13 11:31:20 +0200

    chore/expo: rename root directory, delete old expo app

--- remootio/src/assets/icons.tsx
+++
@@ -1,6 +0,0 @@
-import { AntDesign, Feather } from "@expo/vector-icons";
-
-export const icons = {
-    index: (props: any)=> <AntDesign name="home" size={26} {...props} />,
-    settings: (props: any)=> <Feather name="settings" size={26} {...props} />,
-} as const
